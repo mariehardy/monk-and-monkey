5ea912550d6fb34a0faf04fab3244b752dbd664e
marie-pascale hardy <mariepascalehardy@marie-pascales-MacBook-Pro.local>
2020-03-05 21:28:33 +0100

    BER WDPT Marie - Project 1 - first day

---
+++ canvas.js
@@ -0,0 +1,12 @@
+class Component {
+    constructor(img, width, height, x, y) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.img = img;
+        this.speedX = 0;
+        this.speedY = 0;
+    }
+}
+
---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+        startGame();
+    };
+  }
+  
+  
+  function startGame() {
+    // raceCarCanvas.backgroundImg()
+    // car.update()
+  
+    draw()
+
+  }
