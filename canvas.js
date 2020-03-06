7764aad638ea695bf65349c16b9ecc994f507ec5
marie-pascale hardy <mariepascalehardy@mariepacalesmbp.localdomain>
2020-03-06 17:20:06 +0100

    BERWDPT-Marie-Project1

--- canvas.js
+++ canvas.js
@@ -1,12 +1,54 @@
-class Component {
-    constructor(img, width, height, x, y) {
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-        this.img = img;
-        this.speedX = 0;
-        this.speedY = 0;
-    }
-}
+// var obstacleArr = [];
 
+
+// var myGameArea = {
+//   canvas: document.createElement("canvas"),
+//   start: function () {
+//     this.canvas.width = 480;
+//     this.canvas.height = 270;
+//     this.context = this.canvas.getContext("2d");
+//     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//     this.clear = function () {
+//       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+//     };
+//     // call updateGameArea() every 20 milliseconds
+//     this.interval = setInterval(updateGameArea, 20);
+//   },
+//   stop: function () {
+//     clearInterval(this.interval);
+//   },
+//   score: function() {
+//     var points = Math.floor(this.frames / 5);
+//     this.context.font = "18px serif";
+//     this.context.fillStyle = "black";
+//     this.context.fillText("Score: " + points, 350, 50);
+//   }
+// };
+
+
+
+class GameCanvas {
+    constructor(width, height) {
+      this.canvas = document.getElementById('canvas')
+      this.context = document.getElementById('canvas').getContext('2d');
+      this.canvas.width = width;
+      this.canvas.height = height;
+    }
+  
+    createBoard() {
+      this.context.clearRect(0, 0, this.width, this.height)
+      this.draw()
+    }
+
+    draw() {
+        let monk = new Monk(10, 30, 0, 200);
+        let monkey = new Monkey();
+        console.log(monk.width)
+        monk.update()
+        
+        
+            
+        window.requestAnimationFrame(gameCanvas.draw)
+    }
+
+    }
--- index.js
+++ index.js
@@ -1,16 +1,71 @@
+let canvas = document.getElementById('canvas')
+let context = document.getElementById('canvas').getContext('2d');
+
+class Component {
+  constructor(width, height, x, y) {
+      this.width = width;
+      this.height = height;
+      this.x = x;
+      this.y = y;
+      this.img = new Image();;
+      this.speedX = 0;
+      this.speedY = 0;
+  }
+
+  update() {
+    context.fillStyle = 'red';
+    context.fillRect(50, 50, 60, 60);
+  }
+
+}
+
+class Monk extends Component {
+  constructor(karmaPoint) {
+      super(karmaPoint) // super needs to be specified first, like here
+      this.context = document.getElementById('canvas').getContext('2d');
+
+      this.karmaPoint = karmaPoint
+      this.img = new Image();
+            // img.src = "../images/gameover.png"
+            // this.context.drawImage(img, 20, 100, 100, 10);
+      // function draw(ghost) {
+      this.img.onload = function() {
+        this.img.src = "https://banner2.cleanpng.com/20180325/ute/kisspng-emoji-love-heart-sticker-emoticon-emoji-5ab86fdec2e6d0.1707378915220367027983.jpg";
+        this.context.drawImage(this.img, 200, 200, 50, 50);
+      }
+  }
+
+
+}
+
+
+
+
+class Monkey extends Component {
+  constructor() {
+      super() // super needs to be specified first, like here
+      // this.name = name
+  }
+}
+
+
+
+
+
 
 
 window.onload = () => {
     document.getElementById('start-button').onclick = () => {
-        startGame();
+      gameCanvas = new GameCanvas(800, 640);
+      gameCanvas.createBoard()
     };
   }
   
-  
-  function startGame() {
-    // raceCarCanvas.backgroundImg()
-    // car.update()
-  
-    draw()
 
-  }
+
+
+
+
+// React to user pressing a key
+document.addEventListener('keydown', event => {   // The same as: document.keydown = event => {
+})
